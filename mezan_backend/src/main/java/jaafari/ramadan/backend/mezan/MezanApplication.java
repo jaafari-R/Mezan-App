cf529a0ba1066c08c67438af65d86c56201c62a6
jaafari-R <jaafariramadan@gmail.com>
2024-06-27 19:55:53 +0300

    Initialized backend

---
+++ mezan_backend/src/main/java/jaafari/ramadan/backend/mezan/MezanApplication.java
@@ -0,0 +1,13 @@
+package jaafari.ramadan.backend.mezan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MezanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MezanApplication.class, args);
+	}
+
+}
---
+++ mezan_backend/src/test/java/jaafari/ramadan/backend/mezan/MezanApplicationTests.java
@@ -0,0 +1,13 @@
+package jaafari.ramadan.backend.mezan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MezanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
